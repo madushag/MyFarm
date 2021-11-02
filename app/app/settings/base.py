0eda48ac9793bcd56562d6f33221f0c6608a7686
Chris Rogers <chrxr@outlook.com>
2021-11-02 16:54:12 -0400

    uncommented STATIC_ROOT as it doesn't need to be commented out

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_ROOT = path.join(BASE_DIR, 'static')
+STATIC_ROOT = path.join(BASE_DIR, 'static')
 STATIC_URL = environ.get("STATIC_URL", '/static/')
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
