2a301b52fd092e8279e2a17378b07915164af797
Madusha G <mgamage@rmf.harvard.edu>
2021-11-10 19:24:17 -0500

    - refactored to use forms

--- app/farm/views.py
+++ app/farm/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
+from .form import FarmDetailsForm
 from .models import Farm
 
 
@@ -18,39 +19,49 @@ def details(request, farm_id):
         # filter by user id to prevent information leakage
         farm = Farm.objects.filter(pk=farm_id).filter(farmer=request.user.id).get()
     except Farm.DoesNotExist:
-        return render(request, 'farm/details.html', {'error_message': 'Invalid farm ID'})
+        return render(request, 'farm/details_form.html', {'error_message': 'Invalid farm ID'})
+
     else:
-        return render(request, 'farm/details.html', {'farm': farm})
+        return render(request, 'farm/details_form.html',
+                      {'form': FarmDetailsForm(instance=farm, use_required_attribute=True)})
 
 
 # Handler for adding a new farm
 def add(request):
     if request.method == 'POST':
-        farm = Farm(
-            name=request.POST['name'],
-            description=request.POST['description'],
-            phone_no=request.POST['phone'],
-            farmer=request.user
-        )
-        farm.save()
+        form = FarmDetailsForm(request.POST)
+        if form.is_valid():
+            farm = Farm(
+                name=form.cleaned_data['name'],
+                description=form.cleaned_data['description'],
+                phone_no=form.cleaned_data['phone_no'],
+                farmer=request.user
+            )
+            farm.save()
 
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('farm:index'))
     else:
-        return render(request, 'farm/details.html')
+        form = FarmDetailsForm(use_required_attribute=True)
+        return render(request, 'farm/details_form.html', {'form': form})
 
 
 # Handler for saving details of an existing farm
 def update(request, farm_id):
     farm = get_object_or_404(Farm, pk=farm_id)
 
-    farm.name = request.POST['name']
-    farm.description = request.POST['description']
-    farm.phone_no = request.POST['phone']
+    if request.method == 'POST':
+        form = FarmDetailsForm(request.POST)
 
-    farm.save()
+        if form.is_valid():
+            farm.name = form.cleaned_data['name']
+            farm.description = form.cleaned_data['description']
+            farm.phone_no = form.cleaned_data['phone_no']
+            farm.farmer = request.user
+
+            farm.save()
 
     # Always return an HttpResponseRedirect after successfully dealing
     # with POST data. This prevents data from being posted twice if a
@@ -63,7 +74,7 @@ def delete(request, farm_id):
     try:
         farm = Farm.objects.get(pk=farm_id)
     except Farm.DoesNotExist:
-        return render(request, 'farm/details.html', {'error_message': 'Invalid farm ID'})
+        return render(request, 'farm/details_form.html', {'error_message': 'Invalid farm ID'})
     else:
         farm.delete()
         return HttpResponseRedirect(reverse('farm:index'))
