01529109d0c3d2b432a1acfdaafe26fd4dd34132
Chris Rogers <chrxr@outlook.com>
2021-11-20 17:19:11 -0500

    playing with the config to try and get the s3 storage to work

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -135,18 +135,19 @@ if USE_S3:
   AWS_ACCESS_KEY_ID = environ.get('AWS_ACCESS_KEY_ID', False)
   AWS_SECRET_ACCESS_KEY = environ.get('AWS_SECRET_ACCESS_KEY', False)
   AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+  print(AWS_STORAGE_BUCKET_NAME)
   AWS_DEFAULT_ACL = 'public-read'
   AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
   AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
   # s3 static settings
   AWS_LOCATION = 'static'
-  PUBLIC_MEDIA_LOCATION = 'media'
-  DEFAULT_FILE_STORAGE = 'app.storage_backends.PublicMediaStorage'
-  MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
   STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-  STATICFILES_STORAGE = 'app.storage_backends.StaticStorage'
-  COMPRESS_URL = STATIC_URL
-  COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
+  STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+  # PUBLIC_MEDIA_LOCATION = 'media'
+  # DEFAULT_FILE_STORAGE = 'app.storage_backends.PublicMediaStorage'
+  # MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+  # COMPRESS_URL = STATIC_URL
+  # COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
 else:
   STATIC_ROOT = path.join(BASE_DIR, 'static')
   STATIC_URL = environ.get("STATIC_URL", '/static/')
