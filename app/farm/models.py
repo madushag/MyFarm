58e3210f7f9687728378cd6fb39a732b41a8eccc
Madusha G <mgamage@rmf.harvard.edu>
2021-11-23 12:35:37 -0500

    Interim commit - updated model and migrations

--- app/farm/models.py
+++ app/farm/models.py
@@ -13,7 +13,7 @@ class Farm(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     phone_no = models.CharField(max_length=12, verbose_name="Phone", blank=True, null=True)
-    location_state = models.CharField(max_length=2, verbose_name="State")
+    location_state = models.CharField(max_length=2, verbose_name="State", blank=True, null=True)
     city = models.CharField(max_length=200, blank=True, null=True)
     farmer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
