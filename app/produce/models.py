4a075c09a66c3439092eea1ee0d942eb404896df
Madusha G <mgamage@rmf.harvard.edu>
2021-11-26 22:41:37 -0500

    Adding validations and tests

--- app/produce/models.py
+++ app/produce/models.py
@@ -1,6 +1,7 @@
 import os
 
 from django.conf import settings
+from django.core.validators import MinValueValidator
 from django.db import models
 from farm.models import Farm
 
@@ -23,8 +24,8 @@ name_choices = [
 class Produce(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.FloatField()
-    min_quantity = models.FloatField(verbose_name='Min. Qty')
+    price = models.FloatField(validators=[MinValueValidator(0.0)])
+    min_quantity = models.FloatField(verbose_name='Min. Qty', validators=[MinValueValidator(0.0)])
     is_organic = models.BooleanField(verbose_name='Organic')
     # picture = models.ImageField(upload_to=UploadedConfigPath, blank=True)
     # picture = models.ImageField(upload_to='produce_pics', blank=True)
