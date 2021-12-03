e751e7c32561e7f84630058cc5f92d638f541d24
Madusha G <mgamage@rmf.harvard.edu>
2021-12-03 15:04:10 -0500

    Merged from main

--- app/farm/form.py
+++ app/farm/form.py
@@ -6,7 +6,7 @@ from .models import Farm
 class FarmDetailsForm(ModelForm):
     class Meta:
         model = Farm
-        fields = ['id', 'name', 'farm_name''description', 'phone_no', 'location_state', 'city', 'website_url', 'location_lng', 'location_lat', 'location_name', 'location_url', 'location_address']
+        fields = ['id', 'name', 'farm_name', 'description', 'phone_no', 'location_state', 'city', 'website_url', 'location_lng', 'location_lat', 'location_name', 'location_url', 'location_address']
         widgets = {
             'city': forms.HiddenInput(),
             'location_state': forms.HiddenInput(),
